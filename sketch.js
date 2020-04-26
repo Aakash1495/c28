35237c9f7cd16f7060d58d504d2b950b48539889
swapna maharana <swapnamaharana@swapnas-MacBook-Air.local>
2020-04-26 13:46:59 +0530

    yay

--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ function setup(){
     bird = new Bird(100,100);
 
     log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    slingShot = new Slingshot(bird.body,{x:200,y:100});
 }
 
 function draw(){
@@ -64,5 +64,12 @@ function draw(){
     bird.display();
     platform.display();
     log6.display();
-    chain.display();    
+    slingShot.display();    
+}
+
+function mouseDragged(){
+     Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+     slingShot.fly();
 }
